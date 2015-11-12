3cfc8600406dbfd3eaf8477ea45d6a52b20bbe20
Shaurya Kalsi <shaurya.kalsi@berkeley.edu>
2015-11-12 00:35:55 -0800

    final cleaned up code

--- firewall.py
+++ firewall.py
@@ -38,7 +38,6 @@ class Firewall:
                 self.db_file_dict[country].append(address_range)
         
         # TODO: Also do some initialization if needed.
-        # print self.db_file_dict['AU']
     # @pkt_dir: either PKT_DIR_INCOMING or PKT_DIR_OUTGOING
     # @pkt: the actual data of the IPv4 packet (including IP header)
     def handle_packet(self, pkt_dir, pkt):
@@ -67,15 +66,12 @@ class Firewall:
             source_port_number = struct.unpack('!H', src_port)[0]
             destination_port_number = struct.unpack('!H', dst_port)[0]
             if pkt_dir == PKT_DIR_INCOMING:
-            #print "incoming pkt"
-            #print "external " + source_ip_address
                 if self.match_rules(protocol_number, source_ip_address, source_port_number, packet, pkt_dir):
                     self.send_packet(pkt_dir, pkt)
             elif pkt_dir == PKT_DIR_OUTGOING:
                 if self.match_rules(protocol_number, destination_ip_address, destination_port_number, packet, pkt_dir):
                     self.send_packet(pkt_dir, pkt)
         elif self.protocol_dict['ICMP'] == protocol_number:
-            # print "icmp packet"
             port = packet[0:1]
             port_number, = struct.unpack('!B', port)
             if pkt_dir == PKT_DIR_INCOMING:
@@ -107,7 +103,6 @@ class Firewall:
     # that is within the list_of_lists
     def binary_search(self, val, list_of_lists, start, end):
         if start > end:
-            #print "false"
             return False
         mid = (start + end)/2
         lst = list_of_lists[mid]
@@ -116,13 +111,11 @@ class Firewall:
         elif val > lst[1]:
             return self.binary_search(val, list_of_lists, mid+1, end)
         else:
-            #print"true"
             return True
 
     #returns true if port matches, else false
     def match_port(self, rule_port, external_port):
         #checking for ANY    
-        #print rule_port
         if rule_port.upper() != "ANY":
             #checking for range
             if '-' in rule_port:
@@ -137,15 +130,10 @@ class Firewall:
     # return true if address matches, else false
     def match_address(self, rule_address, external_address):
         # checking for ANY
-        #print "matching address"
-        #print "rule_address: " + str(rule_address)
-        #print "external_address: " + str(external_address)
         if rule_address.upper() != "ANY" and  rule_address != "0.0.0.0/0":
             # check for IP Prefix
-            #print "not ANY"
             if '/' in rule_address:
                 # what to do when its a /0 ?
-                #print "inside /"
                 rule_address_split = rule_address.split('/')
                 rule_address_quad = rule_address_split[0].split('.')
                 if rule_address_split[1] == "0":
@@ -156,43 +144,28 @@ class Firewall:
                 rule_address_mask = rule_address_quad_binary[:int(rule_address_split[1])]
                 external_address_mask = external_address_quad_binary[:int(rule_address_split[1])]
                 if rule_address_mask != external_address_mask:
-                    #print "returning false inside /"
                     return False
             # check for country code
             elif rule_address.upper() in self.db_file_dict:
                 # get list of ip addresses ranges for country
-                #print "in country code case"
-                #print rule_address.upper()
-                #print "inside country code"
                 country_addresses = self.db_file_dict[rule_address.upper()]
-                #print country_addresses
-                #print country_addresses
                 # do binary search on this list
                 external_address_val = int('0b' + ''.join([bin(int(x))[2:].zfill(8) for x in external_address.split('.')]) , 2) 
-                #print "External address: " + str(external_address)
-                #print "External address val: " + str(external_address_val)
                 return self.binary_search(external_address_val, country_addresses, 0, len(country_addresses)-1)
             # check for single IP address
             elif rule_address != external_address:
-                #print "inside single address"
                 return False
         return True
     
     # helper that returns either true for pass or false for drop, based on protocol/ip/port and DNS rules
     def match_rules(self, protocol, external_ip_address, external_port, packet, pkt_dir):
         # check special case DNS
-        #print "protocol number: " + str(protocol)
         is_dns = False
         q_name = []
-        #print self.protocol_dict['UDP'] == protocol
-        #print external_port
-        #print external_port == 53
-        # print protocol
         if self.protocol_dict['UDP'] == protocol and pkt_dir == PKT_DIR_OUTGOING and external_port == 53:   
             dns_data = packet[8:]
             qd_count = dns_data[4:6]
             if struct.unpack('!H',qd_count)[0] == 1:
-                #print "qdcount 1"
                 question = dns_data[12:]
                 i = 0
                 tmp = ''
@@ -205,7 +178,6 @@ class Firewall:
                     q_name.append(tmp)
                     tmp = ''
                     i += number + 1
-                # print q_name
                 #increment i by 1 to get QTYPE
                 i += 1
                 qtype = question[i:i+2]
@@ -221,32 +193,22 @@ class Firewall:
             rule_split = self.rules_file_list[rule].split(' ')
             
             rule_protocol = rule_split[1].upper()
-            # print rule_protocol
             #check and apply dns rules if rule is type dns
             if rule_protocol == "DNS":
-            # print is_dns
                 if is_dns:
-                    #print "in dns case"
                     # name of domain
-                    # print rule_split
                     domain = rule_split[2]
                     # if first character is star, then rest of domain 
                     # should match with any address with same suffix
-                    # print domain
                     if domain[0] == '*':
-                    #print "first is *"
                         domain = domain[1:]
                     # if domain was only '*', then it should match
                     # with ALL addresses
                     if domain != '':
-                        #print "entry not empty"
                         domain_split = domain.split('.')
-                        #print domain_split
                         if domain_split[0] == '':
                             domain_split = domain_split[1:]
-                        # print domain_split
                         if domain_split == q_name[-len(domain_split):]:
-                            #print "in this case"
                             final_index = rule
                             # continue
                     else:
@@ -266,20 +228,14 @@ class Firewall:
                         final_index = rule
             #any rule other than dns, i.e. icmp, tcp, udp
             elif self.protocol_dict[rule_protocol] == protocol:
-                #print "inside protocol match " + str(rule_protocol)
                 #checking external port and checking external address, if they match, this rule correctly applies
-                #print "address " + str(external_ip_address)
-                #print "port " + str(external_port)
                 match_port_result = self.match_port(rule_split[3], external_port) 
                 match_address_result = self.match_address(rule_split[2], external_ip_address)
-                #print "match_port_result: " + str(match_port_result)
-                #print "match_address_result: " + str(match_address_result)
                 if match_port_result and match_address_result:
                     final_index = rule
         if final_index == -1:
             return True
         else:
-            #print "default rule: " + str(self.rules_file_list[final_index])
             return self.rules_file_list[final_index].split(' ')[0].upper()  == "PASS"
 
     # TODO: You can add more methods as you want.
